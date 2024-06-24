a88c17b18018c7e7419200b2ab03b937bdd32c4a
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-24 12:04:53 +0530

    if qr is not null it will generate a qr

--- lib/screens/generate_code.dart
+++ lib/screens/generate_code.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
 
 class GenerateCodeScreen extends StatefulWidget {
   const GenerateCodeScreen({super.key});
@@ -22,12 +23,22 @@ class _GenerateCodeScreenState extends State<GenerateCodeScreen> {
               icon: const Icon(Icons.qr_code_scanner))
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(10.0),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              onSubmitted: (value) {
+                setState(() {
+                  qrData = value;
+                });
+              },
+            ),
+            if (qrData != null) PrettyQrView.data(data: qrData!)
+          ],
         ),
       ),
     );
