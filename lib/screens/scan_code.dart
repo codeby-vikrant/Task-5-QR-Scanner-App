f8cbdd00a0ebb9f30c5037ae4e539d9b23d92201
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-24 11:48:51 +0530

    added a statement for barcode found

--- lib/screens/scan_code.dart
+++ lib/screens/scan_code.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -24,9 +25,13 @@ class _ScanCodeScreenState extends State<ScanCodeScreen> {
       ),
       body: MobileScanner(
           controller: MobileScannerController(
-              detectionSpeed: DetectionSpeed.noDuplicates),
+              detectionSpeed: DetectionSpeed.noDuplicates, returnImage: true),
           onDetect: (capture) {
-            print(capture);
+            final List<Barcode> barcodes = capture.barcodes;
+            final Uint8List? image = capture.image;
+            for (final barcode in barcodes) {
+              print("Barcode found ${barcode.rawValue}");
+            }
           }),
     );
   }
